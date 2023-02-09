41b6f7e3181473dcdecc5f4398273cfbe78b4187
abir364 <abirh364@gmail.com>
2023-02-09 22:45:56 +0600

    react basics

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+// const Person = (props) => {
+//   return (
+//     <>
+//       <h1>Name: {props.name}</h1>
+//       <h2>Surname: {props.surname}</h2>
+//       <h2>Visible: {props.visible}</h2>
+//     </>
+//   );
+// }
+
+const App = () => {
+  // const name = 'jack';
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    // alert('reload');
+    setCounter(100);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h1>Hello, {name}!</h1> */}
+      {/* <Person name="John" surname="Cena" visible="false"/>
+      <Person name="Jane" surname="Doe" visible="true"/>
+      <Person name="Ok" surname="cool" visible="true"/> */}
+      <button onClick={ () => setCounter( (prevCount) => prevCount-1 ) }>-</button>
+      <h1>{counter}</h1>
+      <button onClick={ () => setCounter( (prevCount) => prevCount+1 ) }>+</button>
     </div>
   );
 }
